0545a06f2437850d15193ce463fc0e17255d7255
tompee <tompee@drivemode.com>
2019-11-25 17:00:52 +0900

    Add annotation class

---
+++ annotation/src/main/java/com/tompee/bunch/annotation/Bunch.kt
@@ -0,0 +1,12 @@
+package com.tompee.bunch.annotation
+
+/**
+ * Marks a class as a Bunch class. A bunch is a wrapper around [Bundle] that offers custom accessor
+ * and type safe methods.
+ *
+ * @property name bunch name. This will be a type alias of [Bundle].
+ * @property setters the default setter function name. by default it will contain a single item named with
+ */
+@Target(AnnotationTarget.CLASS)
+@Retention
+annotation class Bunch(val name: String, val setters: Array<String> = [])
